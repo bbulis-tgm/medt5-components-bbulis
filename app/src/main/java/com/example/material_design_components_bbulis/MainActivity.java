1d754feafa32c3d7f5683b0941d9e72fbceb4078
Benjamin Bulis <beni1999@gmx.at>
2021-01-13 16:19:18 +0100

    Working on adding feature to switch

--- app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
+++ app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.material_design_components_bbulis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomAppBar bottomAppBar;
-    FloatingActionButton floatingActionButton;
+    static BottomAppBar bottomAppBar;
+    static FloatingActionButton floatingActionButton;
     TextView counterTextView;
+    SwitchMaterial colorSwitch;
 
     public static int count;
 
@@ -23,14 +30,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.bottomAppBar = findViewById(R.id.bottom_app_bar);
-        this.floatingActionButton = findViewById(R.id.floating_action_button);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        floatingActionButton = findViewById(R.id.floating_action_button);
         this.counterTextView = findViewById(R.id.clicked_counter);
+        this.colorSwitch = findViewById(R.id.color_switcher);
 
-        this.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 count += 1;
+            }
+        });
+
+        this.colorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                System.out.println(b);
+                if (b) {
+                    
+                } else {
+
+                }
             }
         });
 
