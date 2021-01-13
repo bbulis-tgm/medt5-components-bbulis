6459cc4670c107bed71d503dbe90fa277567a861
Benjamin Bulis <beni1999@gmx.at>
2021-01-13 10:54:51 +0100

    Breakpoint commit

--- app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
+++ app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.material_design_components_bbulis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomAppBar bottomAppBar;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.bottomAppBar = findViewById(R.id.bottom_app_bar);
+        this.floatingActionButton = findViewById(R.id.floating_action_button);
+
     }
 }
