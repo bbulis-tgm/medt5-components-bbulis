3f14ea5a20da3a34a28010ba6a8acd3bd9089b31
Benjamin Bulis <beni1999@gmx.at>
2021-01-13 15:30:39 +0100

    Working on Updateing View when clicked

--- app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
+++ app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.material_design_components_bbulis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -10,14 +13,26 @@ public class MainActivity extends AppCompatActivity {
 
     BottomAppBar bottomAppBar;
     FloatingActionButton floatingActionButton;
+    TextView counterTextView;
+
+    public static int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        count = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         this.bottomAppBar = findViewById(R.id.bottom_app_bar);
         this.floatingActionButton = findViewById(R.id.floating_action_button);
+        this.counterTextView = findViewById(R.id.clicked_counter);
+
+        this.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count += 1;
+            }
+        });
 
     }
 }
