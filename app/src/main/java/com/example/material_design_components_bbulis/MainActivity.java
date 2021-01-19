923df89e430553fd16c5cc26d35b511f67f4acb4
Benjamin Bulis <beni1999@gmx.at>
2021-01-19 21:30:51 +0100

    Added IDs and fixed switch issues

--- app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
+++ app/src/main/java/com/example/material_design_components_bbulis/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     static BottomAppBar bottomAppBar;
     static FloatingActionButton floatingActionButton;
     TextView counterTextView;
-    SwitchMaterial colorSwitch;
+    SwitchMaterial sizeSwitcher;
 
     public static int count;
 
@@ -33,23 +33,31 @@ public class MainActivity extends AppCompatActivity {
         bottomAppBar = findViewById(R.id.bottom_app_bar);
         floatingActionButton = findViewById(R.id.floating_action_button);
         this.counterTextView = findViewById(R.id.clicked_counter);
-        this.colorSwitch = findViewById(R.id.color_switcher);
+        this.sizeSwitcher = findViewById(R.id.color_switcher);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                TextView counterView = (TextView) findViewById(R.id.clicked_counter);
                 count += 1;
+                counterTextView.setText(String.format("%d", count));
+                counterView.invalidate();
+                System.out.println(count);
             }
         });
 
-        this.colorSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        this.sizeSwitcher.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 System.out.println(b);
+                TextView counterView = (TextView) findViewById(R.id.clicked_counter);
+                TextView counterLabel = (TextView) findViewById(R.id.clicked_counter_label);
                 if (b) {
-                    
+                    counterView.setTextSize(30);
+                    counterLabel.setTextSize(30);
                 } else {
-
+                    counterView.setTextSize(18);
+                    counterLabel.setTextSize(18);
                 }
             }
         });
